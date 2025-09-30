fded76ac745b5124d99f98bfcfba54a7302c85ac
Esteban Alonzo <pstban10@gmail.com>
2025-09-30 13:09:08 -0300

    versión completada para railway v0.3

--- JsAsesoriaG/settings.py
+++ JsAsesoriaG/settings.py
@@ -118,25 +118,18 @@ WSGI_APPLICATION = 'JsAsesoriaG.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-""" DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-} """
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
-        'USER': 'root',
-        'PASSWORD': 'fnvxaNMZRvvGawRFqVQqncygJdisBHPG',
-        'HOST': 'interchange.proxy.rlwy.net',
-        'PORT': '24235',
+        'USER': env("USER"),
+        'PASSWORD': env("PASSWORD"),
+        'HOST': env("HOST"),
+        'PORT': env("PORT"),
 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
