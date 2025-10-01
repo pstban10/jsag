8f4435058855cdbd593ba8793b8d3a78864f62ce
Esteban Alonzo <pstban10@gmail.com>
2025-09-30 23:29:10 -0300

    arreglando error de ddbb

--- JsAsesoriaG/settings.py
+++ JsAsesoriaG/settings.py
@@ -163,7 +163,12 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
